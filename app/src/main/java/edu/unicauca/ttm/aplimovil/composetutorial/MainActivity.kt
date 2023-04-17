f4bbcd42b1bdddd5e062b1db7aa68a8fd6e4a370
Kevin Herney Vente Angulo <kevinthebestfriends@gmail.com>
2023-04-17 15:26:03 -0500

    Material Design y modo oscuro completado

--- app/src/main/java/edu/unicauca/ttm/aplimovil/composetutorial/MainActivity.kt
+++ app/src/main/java/edu/unicauca/ttm/aplimovil/composetutorial/MainActivity.kt
@@ -1,12 +1,15 @@
 package edu.unicauca.ttm.aplimovil.composetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,13 +17,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import edu.unicauca.ttm.aplimovil.composetutorial.ui.theme.ComposeTutorialTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MassageCard(Mensage("Kevin", "Bienvenido a android con Kotlin"))
+            ComposeTutorialTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MassageCard(Mensage("Kevin", "Bienvenido a android con Kotlin"))
+                }
+            }
 
         }
     }
@@ -39,20 +47,45 @@ fun MassageCard(mgs: Mensage) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
         Column() {
-            Text(text = mgs.autor)
+            Text(
+                text = mgs.autor,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = mgs.cuerpo)
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = mgs.cuerpo,
+                    modifier = Modifier.padding(all = 8.dp),
+                    style = MaterialTheme.typography.body2
+                )
+            }
 
         }
     }
 
 }
 
+//@Preview()
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Modo oscuro"
+)
+
+
 @Preview
 @Composable
 fun PreviewMessageCard() {
-    MassageCard(Mensage("Kevin", "Bienvenido a android con Kotlin"))
+    ComposeTutorialTheme() {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            MassageCard(Mensage("Kevin", "Bienvenido a android con Kotlin"))
+        }
+    }
 }
