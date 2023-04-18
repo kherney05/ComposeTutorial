836c43e311ccfbecf64028d0c3c33afb3420e566
Kevin Herney Vente Angulo <kevinthebestfriends@gmail.com>
2023-04-17 22:17:11 -0500

    Terminamos el instructivo composeTutorial

--- app/src/main/java/edu/unicauca/ttm/aplimovil/composetutorial/MainActivity.kt
+++ app/src/main/java/edu/unicauca/ttm/aplimovil/composetutorial/MainActivity.kt
@@ -4,14 +4,19 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -26,7 +31,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTutorialTheme() {
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    MassageCard(Mensage("Kevin", "Bienvenido a android con Kotlin"))
+                    Conversation(SampleData.conversationSample)
+                    //MessageCard(Message("Kevin", "Bienvenido a android con Kotlin"))
                 }
             }
 
@@ -34,11 +40,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Mensage(val autor: String, val cuerpo: String)
-
+data class Message(val autor: String, val cuerpo: String)
 
 @Composable
-fun MassageCard(mgs: Mensage) {
+fun MessageCard(mgs: Message) {
 
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
@@ -50,7 +55,16 @@ fun MassageCard(mgs: Mensage) {
                 .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
         )
         Spacer(modifier = Modifier.width(8.dp))
-        Column() {
+
+        var isExpanded by remember { mutableStateOf(false) }
+
+
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface,
+        )
+
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
             Text(
                 text = mgs.autor,
                 color = MaterialTheme.colors.secondaryVariant,
@@ -59,24 +73,36 @@ fun MassageCard(mgs: Mensage) {
 
             Spacer(modifier = Modifier.height(4.dp))
 
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
                 Text(
                     text = mgs.cuerpo,
                     modifier = Modifier.padding(all = 8.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
                     style = MaterialTheme.typography.body2
                 )
             }
-
         }
     }
+}
 
+
+@Composable
+fun Conversation(listMessage: List<Message>) {
+    LazyColumn {
+        items(listMessage) { message ->
+            MessageCard(message)
+        }
+    }
 }
 
 //@Preview()
 @Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Modo oscuro"
+    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Modo oscuro"
 )
 
 
@@ -85,7 +111,8 @@ fun MassageCard(mgs: Mensage) {
 fun PreviewMessageCard() {
     ComposeTutorialTheme() {
         Surface(modifier = Modifier.fillMaxSize()) {
-            MassageCard(Mensage("Kevin", "Bienvenido a android con Kotlin"))
+            Conversation(SampleData.conversationSample)
+            //MessageCard(Message("Kevin", "Bienvenido a android con Kotlin"))
         }
     }
 }
